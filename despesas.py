dce029c33fe09c8d9683a59e518fcc5bd59dd855
aleciodalpra <aleciodalpra@gmail.com>
2017-06-18 15:40:46 -0300

    Add files via upload

---
+++ despesas.py
@@ -0,0 +1,15 @@
+#Calculadora de despesas
+
+print 'BALANCO DE DESPESAS'
+alecio = raw_input('Quanto gastou Alecio? ')
+joana = raw_input('Quanto gastou Joana? ')
+total = float(alecio) + float(joana)
+
+print 'Total de gastos: R$ %s.' % total
+
+media = total/2
+
+print 'Media de gasto por pessoa: R$ %s.' % media
+print '.....................'
+print 'ACERTO DE CONTAS'
+
---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# o arquivo tem que estar na pasta /usr/lib/cgi-bin
+
+print 'Content-type: text/html'
+print
+print '<HTML><BODY>'
+print '<H1>Hello World!!!</H1>'
+print '</HTML></BODY>'
